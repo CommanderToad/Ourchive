8f3871bda53897defea6d71da20b9246dea4e7b5
kunkel <kunkel.jack@gmail.com>
2014-06-23 14:43:25 -0700

    Removing resources folder and addit it to git ignore.

--- Resources/iphone/alloy/styles/menu.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-module.exports = [{"isClass":true,"priority":10000.011,"key":"Btn","style":{height:"95px",width:"90%",backgroundColor:"white",top:"10px",}},{"isId":true,"priority":100000.0106,"key":"menuView","style":{layout:"vertical",backgroundColor:"gray",}},{"isId":true,"priority":100000.0107,"key":"title","style":{left:"10px",}},{"isId":true,"priority":100000.0108,"key":"titleView","style":{top:"30px",layout:"horizontal",height:"95px",width:"90%",left:"20px",}},{"isId":true,"priority":100000.0109,"key":"closeBtn","style":{left:"400px",color:"red",}}];
-=======
-module.exports = [{"isClass":true,"priority":10000.0107,"key":"Btn","style":{height:"95px",width:"90%",backgroundColor:"white",top:"10px",}},{"isId":true,"priority":100000.0103,"key":"menuView","style":{layout:"vertical",backgroundColor:"gray",}},{"isId":true,"priority":100000.0104,"key":"title","style":{left:"10px",}},{"isId":true,"priority":100000.0105,"key":"titleView","style":{top:"30px",layout:"horizontal",height:"95px",width:"90%",left:"20px",}},{"isId":true,"priority":100000.0106,"key":"closeBtn","style":{left:"400px",color:"red",}}];
->>>>>>> FETCH_HEAD
