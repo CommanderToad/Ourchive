566b730feea1ce866a5e5e0f33f3c8291346a12f
CommanderToad <kole6393@gmail.com>
2014-06-24 23:15:06 -0700

    fixed stuff

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,9 +15,8 @@ function rowSelect(e) {
 	if (Alloy.Globals.currentView.id != obj.id) {
 		for (var i=0; i < 7 ; i++){
 			if (obj.id == views[i]) {
-				var oldView = currentView;
 				currentView = Alloy.createController(obj.id).getView();
-				$.ds.contentview.remove(currentView);
+				$.ds.contentview.remove(Alloy.Globals.currentView);
 				// if homescreen icon then animate else menu and don't animate
 				if (obj.parent.id == "row1" || obj.parent.id =="row2") {
 					//$.ds.contentview.add(currentView);
@@ -29,7 +28,6 @@ function rowSelect(e) {
    				  //   });
 				} else {
 					$.ds.contentview.add(currentView);
-					$.ds.contentview.remove(currentView);
 				}
 				// set title text
 				$.ds.title.setText(viewTitleText[i]);
