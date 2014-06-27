c1a4a2b0cc40f5f6fcef5c6493e9ef3d420e6a3f
CommanderToad <kole6393@gmail.com>
2014-06-27 16:08:46 -0700

    cancel btw

--- app/controllers/addPhoto.js
+++ app/controllers/addPhoto.js
@@ -1,3 +1,4 @@
+
 var topOverlay = Titanium.UI.createView({
 	width:"100%",
 	height:"110px",
@@ -53,29 +54,35 @@ bottomOverlay.add(cancelBtn);
 button.addEventListener('click', function() {
 	Ti.Media.takePicture();
 });
-galleryBtn.addEventListener('click', function() {
-	Titanium.Media.showCamera({
-	success:function(event) {
-		// called when media returned from the camera
-		
-	},
-	cancel:function() {
-		// called when user cancels taking a picture
-	},
-	error:function(error) {
-		// called when there's an error
-		var a = Titanium.UI.createAlertDialog({title:'Camera'});
-		if (error.code == Titanium.Media.NO_CAMERA) {
-			a.setMessage('Please run this test on device');
-		} else {
-			a.setMessage('Unexpected error: ' + error.code);
-		}
-		a.show();
-	},
-	saveToPhotoGallery:true,
-	allowEditing:true,
-	mediaTypes:[Ti.Media.MEDIA_TYPE_PHOTO]
-});
+galleryBtn.addEventListener('click', function(e) {
+	Titanium.Media.openPhotoGallery({
+		success:function(event) {
+			// called when media returned from the camera
+			var image = event.media;
+			if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+				var imageView = Titanium.UI.createImageView({
+					height:"100%",
+					width:"100%",
+					image: image
+				});
+				$.photo.add(imageView);
+			}
+		},
+		cancel:function() {
+			// called when user cancels taking a picture
+			
+		},
+		error:function(error) {
+			// called when there's an error
+			var a = Titanium.UI.createAlertDialog({title:'Camera'});
+			if (error.code == Titanium.Media.NO_CAMERA) {
+				a.setMessage('Please run this test on device');
+			} else {
+				a.setMessage('Unexpected error: ' + error.code);
+			}
+			a.show();
+		},
+	});
 });
 cancelBtn.addEventListener('click', function() {
 	Ti.Media.hideCamera();
@@ -111,6 +118,7 @@ cancelBtn.addEventListener('click', function() {
 	};
 });
  */
+
 var cameraTransform = Ti.UI.create2DMatrix();
 cameraTransform = cameraTransform.scale(1.325);
 var pics = [];
@@ -159,100 +167,13 @@ Titanium.Media.showCamera({
 	mediaTypes : Ti.Media.MEDIA_TYPE_PHOTO,
 	autohide : false // tell the system not to auto-hide and we'll do it ourself
 });
-/*function openCamera() {
-	
-	var overlayImage = Titanium.UI.createImageView({
-    	width: "100%",
-   		height: "20%",
-   		bottom: "0px",
-    	backgroundColor: 'blue',
-	});
-	var btn = Titanium.UI.createButton({
-		height:"90%",
-		borderRadius:"20px",
-		width:"50%",
-		backgroundColor:"red"
-	});
-	var myOverlay = Titanium.UI.createView();
-	overlayImage.add(btn);
-	myOverlay.add(overlayImage);
-	Titanium.Media.showCamera({
-    	success: function(e) {},
-    	error: function(e) {},
-    	cancel: function(e) {},
-    	overlay: myOverlay,
-    	showControls: false,
-    	saveToPhotoGallery: true,
-    	allowEditing: true,
-	});
-	
-	/*
-	var overlay = Titanium.UI.createView({
-		backgroundColor: 'blue',
-		bottom:0,
-		height: "20%",
-		width: "100%",
-		
-	});
-
-	overlay.add(btn);
-    var cameraOptions = {
-    */
-        /*success : function(event) {
-            var takenPicture = event.media;
-
-            // set image on window
-            $.photo.image = takenPicture;
-
-        },
-        cancel : function() {
-        	Alloy.Globals.contentview.remove(Alloy.Globals.currentView);
-			var	currentView = Alloy.createController("homeView").getView();
-			Alloy.Globals.contentview.add(currentView);
-        	Alloy.Globals.title.setText("Add a Moment");
-            // cancel and close window
-        },*/
-        //overlay: overlay,
-        /*error : function(error) {
-            var a = Ti.UI.createAlertDialog({
-                title : "Camera Error"
-            });
-            if (error.code == Ti.Media.NO_CAMERA) {
-                a.setMessage("MISSING CAMERA");
-            } else {
-                a.setMessage('Unexpected error: ' + error.code);
-            }
-            a.show();
-        },
-        showControls: false
-    };
-    
-    // display camera OR gallery
-    if (Ti.Media.isCameraSupported) {
-        Ti.Media.showCamera(cameraOptions);
-    } else {
-        Ti.Media.openPhotoGallery(cameraOptions);
-    }
-
-};
-
-openCamera();
 
 
-//HintText for the TextArea field.
-if ($.photoDesc.value.length > 0) {
-	$.textAreaHint.hide();
-}
 
-$.photoDesc.addEventListener('change', function(e){
-	if (e.source.value.length > 0) {
-		$.textAreaHint.hide();
-	}
-	else {
-		$.textAreaHint.show();
-	}
+$.cancelBtn.addEventListener('click', function() {
+	Alloy.Globals.contentview.remove(Alloy.Globals.currentView);
+	var currentView = Alloy.createController("homeView").getView();
+	Alloy.Globals.currentView = currentView;
+	Alloy.Globals.contentview.add(currentView);
+	Alloy.Globals.title.setText("Add a Moment");
 });
-
-$.textAreaHint.addEventListener('click', function(e){
-	$.photoDesc.focus();
-});*/
