f52e5a2780342333251a7fbc44a368f56ed68dd2
CommanderToad <kole6393@gmail.com>
2014-06-27 02:22:41 -0700

    photo

--- app/controllers/addPhoto.js
+++ app/controllers/addPhoto.js
@@ -1,25 +1,16 @@
 
-var scanner = Titanium.UI.createView({
-	width : 260,
-	height : 200,
-	borderColor : 'red',
-	borderWidth : 5,
-	borderRadius : 15
+var bottomOverlay = Titanium.UI.createView({
+	width: "100%",
+	height: "20%",
+	bottom: "0px",
+	backgroundColor: 'blue',
 });
  
 var button = Titanium.UI.createButton({
-	color : '#fff',
-	//backgroundImage : '../images/BUTT_grn_on.png',
-	//backgroundSelectedImage : '../images/BUTT_grn_off.png',
-	//backgroundDisabledImage : '../images/BUTT_gry_on.png',
-	bottom : 10,
-	width : 301,
-	height : 57,
-	font : {
-		fontSize : 20,
-		fontWeight : 'bold',
-		fontFamily : 'Helvetica Neue'
-	},
+	height:"90%",
+	borderRadius:"20px",
+	width:"50%",
+	backgroundColor:"red",
 	title : 'Take Picture'
 });
  
@@ -53,7 +44,7 @@ var message = Titanium.UI.createLabel({
 messageView.add(message);
  
 var overlay = Titanium.UI.createView();
-overlay.add(scanner);
+overlay.add(bottomOverlay);
 overlay.add(button);
 overlay.add(messageView);
  
@@ -107,52 +98,41 @@ Titanium.Media.showCamera({
 	mediaTypes : Ti.Media.MEDIA_TYPE_PHOTO,
 	autohide : false // tell the system not to auto-hide and we'll do it ourself
 });
-
-/*	
-var overlayImage = Titanium.UI.createView({
-	width: "100%",
-	height: "20%",
-	bottom: "0px",
-	backgroundColor: 'blue',
-	touchEnabled:true,	
-});
-
-var btn = Titanium.UI.createButton({
-	height:"90%",
-	borderRadius:"20px",
-	width:"50%",
-	backgroundColor:"red",
-	touchEnabled:true,		
-});
-Alloy.Globals.myOverlay = Titanium.UI.createView({
-	width:"100%",
-	height:"100%",
-	touchEnabled: true
-});	
-overlayImage.add(btn);
-Alloy.Globals.myOverlay.add(overlayImage);
-
-btn.addEventListener('click', function(e){
-	console.log("something happened");
-	Titanium.Media.takePicture();
-	 Ti.API.info('click');
-});
-
-Titanium.Media.showCamera({
-	success: function(event) {
-		 var takenPicture = event.media;
-
-        // set image on window
-        $.photo.image = takenPicture;
-	},
-	error: function(e) {},
-	cancel: function(e) {},
-	overlay: Alloy.Globals.myOverlay,
-	showControls: false,
-	saveToPhotoGallery: true,
-	allowEditing: true,
-});
+/*function openCamera() {
+	
+	var overlayImage = Titanium.UI.createImageView({
+    	width: "100%",
+   		height: "20%",
+   		bottom: "0px",
+    	backgroundColor: 'blue',
+	});
+	var btn = Titanium.UI.createButton({
+		height:"90%",
+		borderRadius:"20px",
+		width:"50%",
+		backgroundColor:"red"
+	});
+	var myOverlay = Titanium.UI.createView();
+	overlayImage.add(btn);
+	myOverlay.add(overlayImage);
+	Titanium.Media.showCamera({
+    	success: function(e) {},
+    	error: function(e) {},
+    	cancel: function(e) {},
+    	overlay: myOverlay,
+    	showControls: false,
+    	saveToPhotoGallery: true,
+    	allowEditing: true,
+	});
+	
 	/*
+	var overlay = Titanium.UI.createView({
+		backgroundColor: 'blue',
+		bottom:0,
+		height: "20%",
+		width: "100%",
+		
+	});
 
 	overlay.add(btn);
     var cameraOptions = {
@@ -192,9 +172,26 @@ Titanium.Media.showCamera({
     } else {
         Ti.Media.openPhotoGallery(cameraOptions);
     }
-*/
+
+};
+
+openCamera();
 
 
+//HintText for the TextArea field.
+if ($.photoDesc.value.length > 0) {
+	$.textAreaHint.hide();
+}
 
+$.photoDesc.addEventListener('change', function(e){
+	if (e.source.value.length > 0) {
+		$.textAreaHint.hide();
+	}
+	else {
+		$.textAreaHint.show();
+	}
+});
 
-
+$.textAreaHint.addEventListener('click', function(e){
+	$.photoDesc.focus();
+});*/
