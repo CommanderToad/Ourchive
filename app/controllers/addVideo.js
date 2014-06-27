75e161e491de3ad5e7dd03f3480f408fe6d292f7
kunkel <kunkel.jack@gmail.com>
2014-06-26 23:22:09 -0700

    Let's try this

--- app/controllers/addVideo.js
+++ app/controllers/addVideo.js
@@ -1,12 +1,10 @@
 function openVideoCamera(e) {
 	Titanium.Media.showCamera({
 		success: function(e) {
-			if(e.mediaType=== Titanium.Media.MEDIA_TYPE_VIDEO) {
-				//it's a video & set on window
-				var takenVideo = e.media;
-                movieFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'mymovie.mov');
-                movieFile.write(takenVideo);
-			}
+			//it's a video & set on window
+			var takenVideo = e.media;
+            movieFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'mymovie.mov');
+            movieFile.write(takenVideo);
 		},
 		error: function() {
 			alert("There was an error");
@@ -23,12 +21,16 @@ function openVideoCamera(e) {
         	Alloy.Globals.title.setText("Add a Moment");
         	Alloy.Globals.currentView = currentView;
 		},
-		allowEditing:true,
-		saveToPhotoGallery:true,
-		mediaTypes:[Titanium.Media.MEDIA_TYPE_VIDEO],
-		videoQuality:Titanium.Media.QUALITY_HIGH
+		//saveToPhotoGallery:true,
+		//mediaTypes:[Titanium.Media.MEDIA_TYPE_VIDEO],
+		
+		mediaTypes:   Titanium.Media.MEDIA_TYPE_VIDEO,   //I don't think we need "[]".
+		videoMaximumDuration: 1000000,
+		videoQuality: Titanium.Media.QUALITY_HIGH,
 	});
 }
+
+//Run dat shit.
 openVideoCamera();
 
 
