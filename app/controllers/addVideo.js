c037f970aa0bac8438c7b5618d556b1a246722aa
CommanderToad <kole6393@gmail.com>
2014-06-27 16:55:54 -0700

    video overlay

--- app/controllers/addVideo.js
+++ app/controllers/addVideo.js
@@ -1,25 +1,47 @@
-function openVideoCamera() {
-	Titanium.Media.showCamera( {
-		mediaTypes: 			Titanium.Media.MEDIA_TYPE_VIDEO,
-		videoMaximumDuration: 	10000, //Keep it short for now.
-		videoQuality: 			Titanium.Media.QUALITY_HIGH,
-		saveToPhotoGallery: 	true,
-		allowEditing: 			true,
-		success:function(e) {
-			Ti.Media.setCameraFlashMode(Ti.Media.CAMERA_FLASH_OFF);
-			Ti.Media.saveToPhotoGallery(e.media, {
-				success: function(event) {
-				},
-				error: function(evt) {
-					alert("Failed to save video to gallery");
-				},
-			});
-		},
-		error:function(error) {alert("Error: " + error);},
-	});
-}
 
-openVideoCamera();
+var overlay = Titanium.UI.createView();
+var bottomOverlay = Titanium.UI.createView({
+	width: "100%",
+	height: "150px",
+	bottom: "0px",
+	backgroundColor: 'white',
+});
+var button = Titanium.UI.createButton({
+	height:"90%",
+	borderRadius:"20px",
+	width:"30%",
+	backgroundColor:"red",
+	title : 'Record',
+});
+button.addEventListener('click', function() {
+	Ti.Media.startVideoCapture();
+});
+overlay.add(bottomOverlay);
+bottomOverlay.add(button);
+
+Titanium.Media.showCamera( {
+	mediaTypes: 			Titanium.Media.MEDIA_TYPE_VIDEO,
+	videoMaximumDuration: 	10000, //Keep it short for now.
+	videoQuality: 			Titanium.Media.QUALITY_HIGH,
+	saveToPhotoGallery: 	true,
+	allowEditing: 			true,
+	showControls : false,
+	overlay : overlay,
+	success:function(e) {
+		Ti.Media.setCameraFlashMode(Ti.Media.CAMERA_FLASH_OFF);
+		Ti.Media.saveToPhotoGallery(e.media, {
+			success: function(event) {
+				Ti.Media.hideCamera();
+
+			},
+			error: function(evt) {
+				alert("Failed to save video to gallery");
+			},
+		});
+	},
+	error:function(error) {alert("Error: " + error);},
+});
+
 
 //HintText for the TextArea field.
 if ($.videoDesc.value.length > 0) {
