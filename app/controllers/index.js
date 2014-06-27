9b3e1e98adc8c1be35d8b2534519654ca736b206
CommanderToad <kole6393@gmail.com>
2014-06-26 22:22:17 -0700

    animations

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,10 +4,7 @@ $.homeWin.open();
 Alloy.Globals.contentview = $.ds.contentview;
 Alloy.Globals.title = $.ds.title;
 
-function openFunction() {
-	Alloy.Globals.homeWin = $.homeWin;
-	Alloy.Globals.previous = $.homeWin;
-}
+
 function rowSelect(e) {
 	var obj = e.source;
 	var views = ["homeView", "addText", "addPhoto", "addEvent", "addVideo", "settings", "addAudio"];
@@ -16,20 +13,19 @@ function rowSelect(e) {
 		for (var i=0; i < 7 ; i++){
 			if (obj.id == views[i]) {
 				currentView = Alloy.createController(obj.id).getView();
-				//$.ds.contentview.remove(Alloy.Globals.currentView);
+				
 				// if homescreen icon then animate else menu and don't animate
 				if (obj.parent.id == "row1" || obj.parent.id =="row2") {
 					animation.fadeAndRemove(Alloy.Globals.currentView,500,$.ds.contentview);
 					setTimeout(function(){$.ds.contentview.add(currentView);
 						$.ds.contentview.setOpacity(0);
-					$.ds.contentview.animate({
+						$.ds.contentview.animate({
 						opacity:1,
 						duation:500
 					});},501);
 					
-					
-					//$.ds.contentview.add(currentView);
 				} else {
+					$.ds.contentview.remove(Alloy.Globals.currentView);
 					$.ds.contentview.add(currentView);
 				}
 				// set title text
