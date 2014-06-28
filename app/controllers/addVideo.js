40616c90890c59037a4cbc48bb02ab6908aca287
CommanderToad <kole6393@gmail.com>
2014-06-27 22:08:06 -0700

    video recording

--- app/controllers/addVideo.js
+++ app/controllers/addVideo.js
@@ -1,4 +1,4 @@
-
+var recording = 0;
 var overlay = Titanium.UI.createView();
 var bottomOverlay = Titanium.UI.createView({
 	width: "100%",
@@ -13,23 +13,39 @@ var button = Titanium.UI.createButton({
 	backgroundColor:"red",
 	title : 'Record',
 });
-button.addEventListener('click', function() {
-	Ti.Media.startVideoCapture();
+var cancelBtn = Titanium.UI.createButton({
+	right:"5%",
+	height:"90%",
+	borderRadius:"30px",
+	width:"30%",
+	backgroundColor:"gray",
+	title:"Cancel"
 });
+bottomOverlay.add(cancelBtn);
 overlay.add(bottomOverlay);
 bottomOverlay.add(button);
+button.addEventListener('click', function() {
+	if (recording==0) {
+		Titanium.Media.startVideoCapture();
+		button.title = "Recording...";
+		recording = 1;
+	} else {
+		Titanium.Media.stopVideoCapture();
+		recording = 0;
+	}
+});
 
 Titanium.Media.showCamera( {
 	mediaTypes: 			Titanium.Media.MEDIA_TYPE_VIDEO,
 	videoMaximumDuration: 	10000, //Keep it short for now.
 	videoQuality: 			Titanium.Media.QUALITY_HIGH,
 	saveToPhotoGallery: 	true,
-	allowEditing: 			true,
 	showControls : false,
 	overlay : overlay,
-	success:function(e) {
+	autohide:false,
+	success:function(event) {
 		Ti.Media.setCameraFlashMode(Ti.Media.CAMERA_FLASH_OFF);
-		Ti.Media.saveToPhotoGallery(e.media, {
+		Ti.Media.saveToPhotoGallery(event.media, {
 			success: function(event) {
 				Ti.Media.hideCamera();
 
