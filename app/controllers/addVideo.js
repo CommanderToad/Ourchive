226915c0801426e1628d095ff3090d9ab32dd5ad
CommanderToad <kole6393@gmail.com>
2014-06-28 19:01:46 -0700

    video

--- app/controllers/addVideo.js
+++ app/controllers/addVideo.js
@@ -1,5 +1,13 @@
+var videoFile = 'video/video-test.mp4';
 var recording = 0;
 var overlay = Titanium.UI.createView();
+var topOverlay = Titanium.UI.createView({
+	width:"100%",
+	height:"110px",
+	top:0,
+	layout:"horizontal",
+	backgroundColor:"white"
+});
 var bottomOverlay = Titanium.UI.createView({
 	width: "100%",
 	height: "150px",
@@ -31,6 +39,7 @@ button.addEventListener('click', function() {
 		recording = 1;
 	} else {
 		Titanium.Media.stopVideoCapture();
+		button.title = "Record";
 		recording = 0;
 	}
 });
@@ -47,8 +56,25 @@ Titanium.Media.showCamera( {
 		Ti.Media.setCameraFlashMode(Ti.Media.CAMERA_FLASH_OFF);
 		Ti.Media.saveToPhotoGallery(event.media, {
 			success: function(event) {
-				Ti.Media.hideCamera();
-
+				var video = event.media;
+				 var movieFile = Titanium.Filesystem.getFile(
+						 Titanium.Filesystem.applicationDataDirectory, 
+						 mymovie.mov');
+						 movieFile.write(video);
+ 						videoFile = movieFile.nativePath;
+						Ti.Media.hideCamera();
+						 //create the video player and add it to our window
+						 //note the url property can be a remote url or a local file
+						 var my_movie = Titanium.Media.createVideoPlayer({
+						 url: videoFile,
+						 width: "250px",
+						 height: "250px",
+						 backgroundColor:'#000'
+					 });
+					 $.video.addEventListener('click', function(e){
+						Alloy.Globals.my_movie.play();
+						});
+			 	$.video.add(my_movie);
 			},
 			error: function(evt) {
 				alert("Failed to save video to gallery");
@@ -58,6 +84,15 @@ Titanium.Media.showCamera( {
 	error:function(error) {alert("Error: " + error);},
 });
 
+
+cancelBtn.addEventListener('click', function() {
+	Ti.Media.hideCamera();
+	Alloy.Globals.contentview.remove(Alloy.Globals.currentView);
+	var currentView = Alloy.createController("homeView").getView();
+	Alloy.Globals.currentView = currentView;
+	Alloy.Globals.contentview.add(currentView);
+	Alloy.Globals.title.setText("Add a Moment");
+});
 
 //HintText for the TextArea field.
 if ($.videoDesc.value.length > 0) {
