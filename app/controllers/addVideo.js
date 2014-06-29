5e411cee51111b02f816272c227584d5be289b2a
CommanderToad <kole6393@gmail.com>
2014-06-28 19:50:33 -0700

    Video Thumbnails working

--- app/controllers/addVideo.js
+++ app/controllers/addVideo.js
@@ -1,32 +1,28 @@
-var videoFile = 'video/video-test.mp4';
+var cameraTransform = Ti.UI.create2DMatrix();
+cameraTransform = cameraTransform.scale(1.325);
 var recording = 0;
 var overlay = Titanium.UI.createView();
-var topOverlay = Titanium.UI.createView({
-	width:"100%",
-	height:"110px",
-	top:0,
-	layout:"horizontal",
-	backgroundColor:"white"
-});
 var bottomOverlay = Titanium.UI.createView({
 	width: "100%",
 	height: "150px",
 	bottom: "0px",
-	backgroundColor: 'white',
+	backgroundColor: 'gray',
 });
 var button = Titanium.UI.createButton({
-	height:"90%",
-	borderRadius:"20px",
-	width:"30%",
+	color:"white",
+	height:"140px",
+	borderRadius:"70px",
+	width:"140px",
 	backgroundColor:"red",
 	title : 'Record',
 });
 var cancelBtn = Titanium.UI.createButton({
+	color:"white",
 	right:"5%",
-	height:"90%",
-	borderRadius:"30px",
-	width:"30%",
-	backgroundColor:"gray",
+	height:"120px",
+	borderRadius:"60px",
+	width:"120px",
+	backgroundColor:"blue",
 	title:"Cancel"
 });
 bottomOverlay.add(cancelBtn);
@@ -52,29 +48,29 @@ Titanium.Media.showCamera( {
 	showControls : false,
 	overlay : overlay,
 	autohide:false,
+	transform:cameraTransform,
 	success:function(event) {
 		Ti.Media.setCameraFlashMode(Ti.Media.CAMERA_FLASH_OFF);
 		Ti.Media.saveToPhotoGallery(event.media, {
 			success: function(event) {
-				var video = event.media;
-				 var movieFile = Titanium.Filesystem.getFile(
-						 Titanium.Filesystem.applicationDataDirectory, 
-						 mymovie.mov');
-						 movieFile.write(video);
- 						videoFile = movieFile.nativePath;
-						Ti.Media.hideCamera();
+				Ti.Media.hideCamera();
+						var video = event.media;
+						movieFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'mymovie.mov');
+               			 movieFile.write(video);
 						 //create the video player and add it to our window
-						 //note the url property can be a remote url or a local file
-						 var my_movie = Titanium.Media.createVideoPlayer({
-						 url: videoFile,
+						var my_movie = Titanium.Media.createVideoPlayer({
+						 media:movieFile,
+						 movieControlMode:Titanium.Media.VIDEO_CONTROL_DEFAULT,
+						 scalingMode:Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
 						 width: "250px",
 						 height: "250px",
-						 backgroundColor:'#000'
+						 backgroundColor:'#000',
+						 autoplay : false,
 					 });
+					 $.video.add(my_movie);
 					 $.video.addEventListener('click', function(e){
-						Alloy.Globals.my_movie.play();
-						});
-			 	$.video.add(my_movie);
+						my_movie.play();
+					});
 			},
 			error: function(evt) {
 				alert("Failed to save video to gallery");
@@ -110,4 +106,13 @@ $.videoDesc.addEventListener('change', function(e){
 
 $.textAreaHint.addEventListener('click', function(e){
 	$.videoDesc.focus();
+});
+
+// Edit Details Buttons
+$.cancelBtn.addEventListener('click', function(e){
+	Alloy.Globals.contentview.remove(Alloy.Globals.currentView);
+	var currentView = Alloy.createController("homeView").getView();
+	Alloy.Globals.currentView = currentView;
+	Alloy.Globals.contentview.add(currentView);
+	Alloy.Globals.title.setText("Add a Moment");
 });
