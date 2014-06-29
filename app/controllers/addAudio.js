3eab9fba7bcb8a2608693c196184157fc7709466
CommanderToad <kole6393@gmail.com>
2014-06-29 14:57:57 -0700

    audio stuff

--- app/controllers/addAudio.js
+++ app/controllers/addAudio.js
@@ -7,5 +7,51 @@ recording.format = Ti.Media.AUDIO_FILEFORMAT_WAVE;
  
 $.record.addEventListener('click', function(e) {
     recording.start();
+    Ti.Media.startMicrophoneMonitor();
     $.record.setText("Recording");
+});
+
+$.stop.addEventListener('click', function(e) {
+     // here it stops recording
+            var recordedFile = recording.stop();
+             
+            // if it doesn't exist I create a directory containing recorded audio files
+            var audioDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'recorded_audio_files');
+            if (!audioDir.exists()) {audioDir.createDirectory();};
+             
+            // I write the most recent recording into a file
+            // getScrollableViewStatus() creates a UNIQUE name for that file
+            var rec = Titanium.Filesystem.getFile(audioDir.resolve(),'key.wav');
+             
+            // error check
+            if (rec.write(recordedFile)===false) {alert("writing issue");};
+             
+            // currentKey contains the same value as getScrollableViewStatus() above
+            // and it becomes a unique key for a Property (it CAN be a key as it is a character sequence 'ABCDEF')
+            var currentKey = 'key.wav';
+            // the corresponding value is the nativePath of the saved sound
+            var currentValue = rec.nativePath;
+             
+            // I get the object sounds and I create an empty object if it doen't exist
+            var obj = Ti.App.Properties.getObject("sounds")||{};
+            // I assign to tempObj the current value of obj
+            var tempObj = obj;
+            // I add a key/value pair to my object
+            tempObj[currentKey] = currentValue;
+            // I store the object in sounds
+            Ti.App.Properties.setObject("sounds", tempObj);
+ 
+});
+
+$.play.addEventListener('click', function(e) {
+	Titanium.Media.AUDIO_SESSION_MODE_PLAYBACK;
+   var sounds = Titanium.App.Properties.getObject("sounds");
+var currentKey = 'key.wav';
+var soundPath = sounds[currentKey];
+var fileToPlay = Titanium.Filesystem.getFile(soundPath);
+ 
+var sound = Titanium.Media.createSound({
+    url : fileToPlay
+});
+sound.play();
 });
