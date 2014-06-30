7b2108fb904263c044fcaf2886cdc2667bcd38d7
kunkel <kunkel.jack@gmail.com>
2014-06-29 20:38:53 -0700

    FUCK SHIT STACK BEEOOOTCH

--- app/controllers/addAudio.js
+++ app/controllers/addAudio.js
@@ -6,65 +6,58 @@ recording.compression = Ti.Media.AUDIO_FORMAT_ULAW;
 recording.format = Ti.Media.AUDIO_FILEFORMAT_WAVE;  
  
 $.record.addEventListener('click', function(e) {
-    recording.start();
-    Ti.Media.startMicrophoneMonitor();
-    $.record.setText("Recording");
-});
-
-$.stop.addEventListener('click', function(e) {
-     // here it stops recording
-            var recordedFile = recording.stop();
-             
-            // if it doesn't exist I create a directory containing recorded audio files
-            var audioDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'recorded_audio_files');
-            if (!audioDir.exists()) {audioDir.createDirectory();};
-             
-            // I write the most recent recording into a file
-            // getScrollableViewStatus() creates a UNIQUE name for that file
-            var rec = Titanium.Filesystem.getFile(audioDir.resolve(),'key.wav');
-             
-            // error check
-            if (rec.write(recordedFile)===false) {alert("writing issue");};
-             
-            // currentKey contains the same value as getScrollableViewStatus() above
-            // and it becomes a unique key for a Property (it CAN be a key as it is a character sequence 'ABCDEF')
-            var currentKey = 'key.wav';
-            // the corresponding value is the nativePath of the saved sound
-            var currentValue = rec.nativePath;
-             
-            // I get the object sounds and I create an empty object if it doen't exist
-            var obj = Ti.App.Properties.getObject("sounds")||{};
-            // I assign to tempObj the current value of obj
-            var tempObj = obj;
-            // I add a key/value pair to my object
-            tempObj[currentKey] = currentValue;
-            // I store the object in sounds
-            Ti.App.Properties.setObject("sounds", tempObj);
- 
+	if (e.source.image === "images/record-start.png") {
+		recording.start();
+    	Ti.Media.startMicrophoneMonitor();
+    	e.source.image = "images/record-stop.png";	
+	}
+	else {
+		e.source.image = "images/record-start.png";
+		// here it stops recording
+    	var recordedFile = recording.stop(); 
+    	// if it doesn't exist I create a directory containing recorded audio files
+    	var audioDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'recorded_audio_files');
+    	if (!audioDir.exists()) {
+    		audioDir.createDirectory();
+    	};       
+    	// I write the most recent recording into a file
+    	// getScrollableViewStatus() creates a UNIQUE name for that file
+    	var rec = Titanium.Filesystem.getFile(audioDir.resolve(),'key.wav');
+    	// error check
+    	if (rec.write(recordedFile)===false) {
+    		alert("writing issue");
+    	};
+    	// currentKey contains the same value as getScrollableViewStatus() above
+    	// and it becomes a unique key for a Property (it CAN be a key as it is a character sequence 'ABCDEF')
+    	var currentKey = 'key.wav';
+    	// the corresponding value is the nativePath of the saved sound
+    	var currentValue = rec.nativePath; 
+    	// I get the object sounds and I create an empty object if it doen't exist
+    	var obj = Ti.App.Properties.getObject("sounds")||{};
+    	// I assign to tempObj the current value of obj
+    	var tempObj = obj;
+    	// I add a key/value pair to my object
+    	tempObj[currentKey] = currentValue;
+    	// I store the object in sounds
+    	Ti.App.Properties.setObject("sounds", tempObj);
+	}   
 });
 
 $.play.addEventListener('click', function(e) {
 	Titanium.Media.audioSessionMode = Ti.Media.AUDIO_SESSION_MODE_PLAYBACK;
-   var sounds = Titanium.App.Properties.getObject("sounds");
-var currentKey = 'key.wav';
-var soundPath = sounds[currentKey];
-var fileToPlay = Titanium.Filesystem.getFile(soundPath);
- 
-<<<<<<< HEAD
-$.recordStartBtn.addEventListener('click', function(e) {
-    if (recorder.recording) {           
-        var recordedFile = recorder.stop();
-        var voiceFile = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'test.m4a');
-        voiceFile.write(recordedFile);
-    } else {
-        recorder.start();
-    }
+   	var sounds = Titanium.App.Properties.getObject("sounds");
+	var currentKey = 'key.wav';
+	var soundPath = sounds[currentKey];
+	var fileToPlay = Titanium.Filesystem.getFile(soundPath);
+	var sound = Titanium.Media.createSound({
+    	url : fileToPlay
+	});
+	if (e.source.image = "images/record-play.png") {
+		e.source.image = "images/record-pause.png";
+		sound.play();
+	}
+	else {
+		e.source.image = "images/record-play.png";
+		sound.pause();
+	}
 });
-
-=======
-var sound = Titanium.Media.createSound({
-    url : fileToPlay
-});
-sound.play();
-});
->>>>>>> FETCH_HEAD
